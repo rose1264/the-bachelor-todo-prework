cb92a995a02670d122d37c993baa36b880e69a92
Shumei Huang <rose1264@gmail.com>
2018-06-12 16:18:52 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,23 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |person|
+    if person["status"] == "Winner"
+      return person["name"].split(" ").first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+
 end
 
 def get_occupation(data, hometown)
-  # code here
+
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+
 end
